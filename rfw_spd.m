e32285f1e6e6f707f2b3c465ccea913a1782b168
windows7lover <damien.scieur@gmail.com>
2025-11-27 15:06:45 -0500

    .

--- rfw_spd.m
+++ rfw_spd.m
@@ -1,21 +1,4 @@
 % -------------------------------------------------------------------------
-% Localized free-energy minimization on the SPD manifold
-% -------------------------------------------------------------------------
-% Energy with dimension-mixing interactions:
-%
-%     E(X) = 1/2 * Tr(A X B X)  +  Tr(C X)
-%
-% where A,B,C are symmetric matrices (A,B ? 0 for stability).
-%
-% Gradient:
-%
-%     ?E(X) = 1/2 * (A X B + B X A) + C
-%
-% We linearize only the energy term around X0 and keep entropy S(X) exact.
-%
-% Solve the local trust-region subproblem:
-%
-%     minimize_X    < ?E(X0),  X - X0 >_X  -  T * S(X)% -------------------------------------------------------------------------
 % Simplified RFW on a ball in the SPD Manifold
 % -------------------------------------------------------------------------
 % INPUT:
@@ -50,30 +33,3 @@ function X = rfw_spd(f, Xbar, r, max_iter)
 
     end
 end
-
-%     subject to    d(X, X0) ? r
-%                   X ? S_{++}^n
-%
-% where:
-%   - d(·,·) is the affine-invariant Riemannian distance,
-%   - S(X)  = -Tr[ X log(X) + (I-X) log(I-X) ],
-%   - r > 0 is the trust-region radius.
-%
-% -------------------------------------------------------------------------
-
-% Example entropy
-function val = entropy_S(X)    I = eye(size(X));
-    val = -trace( X*logm(X) + (I-X)*logm(I-X) );
-end
-
-% Example energy ingredients
-A = ...;   % symmetric positive definite
-B = ...;   % symmetric positive definite
-C = ...;   % symmetric
-
-X0 = ...;  % current SPD matrix
-T  = ...;  % temperature
-r  = ...;  % trust-region radius
-
-% Gradient of E at X0:
-gradE_X0 = 0.5 * (A*X0*B + B*X0*A) + C;
